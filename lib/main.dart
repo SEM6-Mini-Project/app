4c70b13193b98ba998ee75ebf2641614bf992e43
iamkunal2k <kunalkumbhare8@gmail.com>
2023-04-05 02:24:54 +0530

    authentication and signup page is completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:prediction/screens/prediction.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:prediction/screens/sign_in_screen.dart';
 
-void main() {
-  runApp(LoginUiApp());
+Future<void> main() async {
+  FirebaseApp myapp = await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyB2pR40icU76TNgB0k1_qphNZIK4M7vu3M",
+        authDomain: "bankruptcy-prediction-f1817.firebaseapp.com",
+        databaseURL:
+            "https://bankruptcy-prediction-f1817-default-rtdb.firebaseio.com",
+        projectId: "bankruptcy-prediction-f1817",
+        storageBucket: "bankruptcy-prediction-f1817.appspot.com",
+        messagingSenderId: "207241405329",
+        appId: "1:207241405329:web:07f0d178a3bdc2748ef030",
+        measurementId: "G-Z9K70N1TPW"),
+  );
+  runApp(MaterialApp(
+    home: LoginUiApp(),
+  ));
 }
 
 class LoginUiApp extends StatelessWidget {
@@ -33,7 +49,7 @@ class LoginUiApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.grey.shade100,
         primarySwatch: Colors.grey,
       ),
-      home: const PredictionPage(),
+      home: const SignInScreen(),
     );
   }
 }
