1b07e309e87a8ddd432324cba2c9bb4e3ab5f922
iamkunal2k <kunalkumbhare8@gmail.com>
2023-04-05 11:51:05 +0530

    authentication of android is done

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class LoginUiApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.grey.shade100,
         primarySwatch: Colors.grey,
       ),
-      home: Dashboard(),
+      home: SignInScreen(),
     );
   }
 }
